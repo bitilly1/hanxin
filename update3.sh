57802a75d5b8de141a4798c6dba446f5268a362d
hanxin <weku@foxmail.com>
2025-03-15 20:23:40 +0800

    Update all files

---
+++ update3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+_do(){
+	echo "now: ${1}"
+	cd "${1}"
+	echo 'remove old Packages file'
+	rm -rfv Packages*
+	echo 'new Packages file being generated'
+	dpkg-scanpackages -m ./debs /dev/null >Packages &
+	bzip2 -v -k -9 Packages &
+	xz -v -k -9 --extreme Packages &
+	#zstd --ultra -22 -k Packages -o Packages.zst
+}
+
+_do '/var/mobile/hanxin/'
